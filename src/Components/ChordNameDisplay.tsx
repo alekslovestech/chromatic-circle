600ceb4d0240d0af6d8f861fcb79d96f7d464a29
Aleks Dubinskiy <71671601+alekslovestech@users.noreply.github.com>
2024-08-20 09:36:47 +0200

    Feature/layouts (#6)
    * extract ChordNameDisplay
    Arrange components on a grid

---
+++ src/Components/ChordNameDisplay.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useNotes } from './NotesContext';
+import { getChordName, getNoteTextFromIndex } from '../utils/ChromaticUtils';
+import '../styles/ChordNameDisplay.css'; 
+
+const ChordDisplay: React.FC = () => {
+  const {
+    selectedNoteIndices,
+    selectedChordType,
+    selectedAccidental,
+  } = useNotes();
+
+  return (
+    <div className="chord-display">
+      <div className="chord-name">
+        chord:{' '}
+        {selectedNoteIndices.length === 0
+          ? 'UNKNOWN'
+          : getChordName(
+              selectedNoteIndices[0],
+              selectedChordType,
+              selectedAccidental
+            )}
+      </div>
+      <br/>
+      <div className="chord-notes">
+        notes:{' '}
+        {selectedNoteIndices
+          .map((index) => getNoteTextFromIndex(index, selectedAccidental))
+          .join('-')}
+      </div>
+    </div>
+  );
+};
+
+export default ChordDisplay;
