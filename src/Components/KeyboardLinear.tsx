9ee1d4d38e88e3a2f3b35c1c44c9f9513a014952
Aleks LovesTech <alekslovestech@gmail.com>
2024-09-24 09:12:51 +0200

    moving color logic to css

--- src/Components/KeyboardLinear.tsx
+++ src/Components/KeyboardLinear.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import "../styles/KeyboardLinear.css";
 import { useNotes } from "./NotesContext";
-import {
-  getBlackWhiteString,
-  getComputedColor,
-  getComputedKeyColor,
-  getComputedTextColor,
-} from "../utils/ColorUtils";
 import { TWELVE } from "../types/NoteConstants";
 import { ActualIndex } from "../types/IndexTypes";
 import { getNoteTextFromIndex } from "../utils/NoteUtils";
 import { useKeyboardHandlers } from "./useKeyboardHandlers";
+import { IndexUtils } from "../utils/IndexUtils";
 
 const KeyboardLinear: React.FC = () => {
   const { selectedNoteIndices, selectedAccidental } = useNotes();
@@ -20,16 +15,19 @@ const KeyboardLinear: React.FC = () => {
   for (let actualIndex = 0 as ActualIndex; actualIndex < 2 * TWELVE; actualIndex++) {
     const isSelected = selectedNoteIndices.includes(actualIndex);
     const isRootNote = checkIsRootNote(actualIndex);
+    const isBlack = IndexUtils.isBlackKey(actualIndex);
+    const blackWhiteString = isBlack ? "black" : "white";
+
+    //later classes override earlier ones
+    const classNames = ["piano-key"];
+    classNames.push(blackWhiteString);
+    if (isSelected) classNames.push("selected");
+    if (isRootNote) classNames.push("root-note");
 
     keys.push(
       <div
         key={actualIndex}
-        className={`piano-key ${getBlackWhiteString(actualIndex)}`}
-        style={{
-          backgroundColor: getComputedKeyColor(actualIndex, isSelected),
-          color: getComputedTextColor(actualIndex),
-          border: isRootNote ? `2px solid ${getComputedColor("--root-note-highlight")}` : undefined,
-        }}
+        className={classNames.join(" ")}
         onClick={() => handleKeyClick(actualIndex)}
       >
         {getNoteTextFromIndex(actualIndex, selectedAccidental)}
