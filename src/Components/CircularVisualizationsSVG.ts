bd7f6d671701b4322afae3f4fa1c2c2f9d090623
Aleks LovesTech <alekslovestech@gmail.com>
2024-10-02 13:47:51 +0200

    moved SVG circular visualizations for pie slice to their own file

---
+++ src/Components/CircularVisualizationsSVG.ts
@@ -0,0 +1,120 @@
+import { ActualIndex } from "../types/IndexTypes";
+import { TWELVE } from "../types/NoteConstants";
+import { getComputedColor } from "../utils/ColorUtils";
+import { CommonMath, INNER_RADIUS } from "../utils/CommonMath";
+import { PolarMath } from "../utils/PolarMath";
+import { CircularVisMode } from "./CircularVisualizations";
+
+const SVG_URL = "http://www.w3.org/2000/svg";
+
+export function drawCircularVisualizationsSVG(
+  selectedNoteIndices: ActualIndex[],
+  circularVisMode: CircularVisMode,
+) {
+  // Remove existing lines
+  const svgElement = document.querySelector(".pie-slice-keyboard");
+  if (svgElement) {
+    svgElement.querySelectorAll(".selected-note-line").forEach((el) => el.remove());
+  }
+
+  // Only draw visualizations if there's more than one selected note
+  if (selectedNoteIndices.length > 1) {
+    if (circularVisMode === CircularVisMode.Arrows) drawSelectedNotesArrows(selectedNoteIndices);
+    else if (circularVisMode === CircularVisMode.Polygon)
+      drawSelectedNotesPolygon(selectedNoteIndices);
+  }
+}
+
+function drawSelectedNotesArrows(selectedNoteIndices: ActualIndex[]) {
+  const numNotes = selectedNoteIndices.length;
+  if (numNotes < 2) return;
+  const svgElement = document.querySelector(".pie-slice-keyboard");
+  if (!svgElement) return;
+
+  // Remove existing lines
+  svgElement.querySelectorAll(".selected-note-line").forEach((el) => el.remove());
+
+  selectedNoteIndices.forEach((index) => {
+    const { middleAngle } = CommonMath.NoteIndexToAngles(index);
+    const innerPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngle);
+
+    const path = document.createElementNS(SVG_URL, "path");
+    path.setAttribute("d", `M0,0 L${innerPoint.x},${innerPoint.y}`);
+    path.setAttribute("stroke", getComputedColor("--key-border"));
+    path.setAttribute("stroke-width", "2");
+    path.classList.add("selected-note-line");
+
+    svgElement.appendChild(path);
+  });
+
+  // Emphasize the base note
+  if (selectedNoteIndices.length > 0) {
+    const baseIndex = selectedNoteIndices[0];
+    const { middleAngle } = CommonMath.NoteIndexToAngles(baseIndex);
+    const innerPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngle);
+
+    const circle = document.createElementNS(SVG_URL, "circle");
+    circle.setAttribute("cx", innerPoint.x.toString());
+    circle.setAttribute("cy", innerPoint.y.toString());
+    circle.setAttribute("r", "5");
+    circle.setAttribute("fill", getComputedColor("--root-note-highlight"));
+    circle.setAttribute("stroke", getComputedColor("--key-border"));
+    circle.setAttribute("stroke-width", "1");
+    circle.classList.add("selected-note-line");
+
+    svgElement.appendChild(circle);
+  }
+}
+
+function drawSelectedNotesPolygon(selectedNoteIndices: ActualIndex[]) {
+  const numNotes = selectedNoteIndices.length;
+  const svgElement = document.querySelector(".pie-slice-keyboard");
+  if (!svgElement || numNotes < 2) return;
+
+  // Remove existing lines
+  svgElement.querySelectorAll(".selected-note-line").forEach((el) => el.remove());
+
+  const theEnd = numNotes === 2 ? 1 : numNotes; // intervals don't wrap around, but chords do
+
+  for (let i = 0; i < theEnd; i++) {
+    const currentIndex = selectedNoteIndices[i];
+    const nextIndex = selectedNoteIndices[(i + 1) % numNotes];
+
+    const { middleAngle: middleAngleCur } = CommonMath.NoteIndexToAngles(currentIndex);
+
+    const { middleAngle: middleAngleNext } = CommonMath.NoteIndexToAngles(nextIndex);
+
+    const startPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngleCur);
+    const endPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngleNext);
+
+    const path = document.createElementNS(SVG_URL, "path");
+    path.setAttribute("d", `M${startPoint.x},${startPoint.y} L${endPoint.x},${endPoint.y}`);
+
+    const noteDistance = CommonMath.noteDistance(currentIndex, nextIndex);
+    const hue = (noteDistance / TWELVE) * 240; // Map note distance from red (0) to blue (240)
+    path.setAttribute("stroke", `hsl(${hue}, 100%, 50%)`);
+
+    path.setAttribute("stroke-width", "2");
+    path.classList.add("selected-note-line");
+
+    svgElement.appendChild(path);
+  }
+
+  // Emphasize the base note
+  if (selectedNoteIndices.length > 0) {
+    const baseIndex = selectedNoteIndices[0];
+    const { middleAngle } = CommonMath.NoteIndexToAngles(baseIndex);
+    const innerPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngle);
+
+    const circle = document.createElementNS(SVG_URL, "circle");
+    circle.setAttribute("cx", innerPoint.x.toString());
+    circle.setAttribute("cy", innerPoint.y.toString());
+    circle.setAttribute("r", "5");
+    circle.setAttribute("fill", getComputedColor("--root-note-highlight"));
+    circle.setAttribute("stroke", getComputedColor("--key-border"));
+    circle.setAttribute("stroke-width", "1");
+    circle.classList.add("selected-note-line");
+
+    svgElement.appendChild(circle);
+  }
+}
--- src/Components/KeyboardPieSlice.tsx
+++ src/Components/KeyboardPieSlice.tsx
@@ -3,14 +3,14 @@ import "../styles/KeyboardPieSlice.css";
 import { TWELVE } from "../types/NoteConstants";
 import { ActualIndex } from "../types/IndexTypes";
 import { getNoteTextFromIndex } from "../utils/NoteUtils";
-import { getBlackWhiteString, getComputedColor } from "../utils/ColorUtils";
+import { getBlackWhiteString } from "../utils/ColorUtils";
 import { useKeyboardHandlers } from "./useKeyboardHandlers";
 import { useNotes } from "./NotesContext";
 import AccidentalToggle from "./AccidentalToggle";
 import CircularVisModeSelect from "./CircularVizModeSelect";
-import { CircularVisMode } from "./CircularVisualizations";
 import { PolarMath } from "../utils/PolarMath";
 import { CommonMath, INNER_RADIUS, MIDDLE_RADIUS, OUTER_RADIUS } from "../utils/CommonMath";
+import { drawCircularVisualizationsSVG } from "./CircularVisualizationsSVG";
 
 interface PieSliceProps {
   index: number;
@@ -57,107 +57,9 @@ const KeyboardPieSlice: React.FC = () => {
   };
 
   useEffect(() => {
-    function drawSelectedNotesArrows(selectedNoteIndices: ActualIndex[]) {
-      const svgElement = document.querySelector(".pie-slice-keyboard");
-      if (!svgElement) return;
-
-      // Remove existing lines
-      svgElement.querySelectorAll(".selected-note-line").forEach((el) => el.remove());
-
-      selectedNoteIndices.forEach((index) => {
-        const { middleAngle } = CommonMath.NoteIndexToAngles(index);
-        const innerPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngle);
-
-        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute("d", `M0,0 L${innerPoint.x},${innerPoint.y}`);
-        path.setAttribute("stroke", getComputedColor("--key-border"));
-        path.setAttribute("stroke-width", "2");
-        path.classList.add("selected-note-line");
-
-        svgElement.appendChild(path);
-      });
-
-      // Emphasize the base note
-      if (selectedNoteIndices.length > 0) {
-        const baseIndex = selectedNoteIndices[0];
-        const { middleAngle } = CommonMath.NoteIndexToAngles(baseIndex);
-        const innerPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngle);
-
-        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        circle.setAttribute("cx", innerPoint.x.toString());
-        circle.setAttribute("cy", innerPoint.y.toString());
-        circle.setAttribute("r", "5");
-        circle.setAttribute("fill", getComputedColor("--root-note-highlight"));
-        circle.setAttribute("stroke", getComputedColor("--key-border"));
-        circle.setAttribute("stroke-width", "1");
-        circle.classList.add("selected-note-line");
-
-        svgElement.appendChild(circle);
-      }
-    }
-
-    function drawSelectedNotesPolygon(selectedNoteIndices: ActualIndex[]) {
-      const svgElement = document.querySelector(".pie-slice-keyboard");
-      if (!svgElement || selectedNoteIndices.length < 2) return;
-
-      // Remove existing lines
-      svgElement.querySelectorAll(".selected-note-line").forEach((el) => el.remove());
-
-      const numNotes = selectedNoteIndices.length;
-      const theEnd = numNotes === 2 ? 1 : numNotes; // intervals don't wrap around, but chords do
-
-      for (let i = 0; i < theEnd; i++) {
-        const currentIndex = selectedNoteIndices[i];
-        const nextIndex = selectedNoteIndices[(i + 1) % numNotes];
-
-        const { middleAngle: middleAngleCur } = CommonMath.NoteIndexToAngles(currentIndex);
-
-        const { middleAngle: middleAngleNext } = CommonMath.NoteIndexToAngles(nextIndex);
-
-        const startPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngleCur);
-        const endPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngleNext);
-
-        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute("d", `M${startPoint.x},${startPoint.y} L${endPoint.x},${endPoint.y}`);
-
-        const noteDistance = CommonMath.noteDistance(currentIndex, nextIndex);
-        const hue = (noteDistance / TWELVE) * 240; // Map note distance from red (0) to blue (240)
-        path.setAttribute("stroke", `hsl(${hue}, 100%, 50%)`);
-
-        path.setAttribute("stroke-width", "2");
-        path.classList.add("selected-note-line");
-
-        svgElement.appendChild(path);
-      }
-
-      // Emphasize the base note
-      if (selectedNoteIndices.length > 0) {
-        const baseIndex = selectedNoteIndices[0];
-        const { middleAngle } = CommonMath.NoteIndexToAngles(baseIndex);
-        const innerPoint = PolarMath.getCartesianFromPolar(INNER_RADIUS, middleAngle);
-
-        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        circle.setAttribute("cx", innerPoint.x.toString());
-        circle.setAttribute("cy", innerPoint.y.toString());
-        circle.setAttribute("r", "5");
-        circle.setAttribute("fill", getComputedColor("--root-note-highlight"));
-        circle.setAttribute("stroke", getComputedColor("--key-border"));
-        circle.setAttribute("stroke-width", "1");
-        circle.classList.add("selected-note-line");
-
-        svgElement.appendChild(circle);
-      }
-    }
-    function drawCircularVisualizations() {
-      if (circularVisMode === CircularVisMode.Arrows) {
-        drawSelectedNotesArrows(selectedNoteIndices);
-      } else if (circularVisMode === CircularVisMode.Polygon) {
-        drawSelectedNotesPolygon(selectedNoteIndices);
-      }
-    }
-
-    drawCircularVisualizations();
+    drawCircularVisualizationsSVG(selectedNoteIndices, circularVisMode);
   }, [selectedNoteIndices, handleKeyClick, circularVisMode]);
+
   return (
     <div className="keyboardpieslice-container">
       <div className="keyboardpieslice-overlay">
