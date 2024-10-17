234ff70467c555226c8db767abd73a4b4ab68fc4
Aleks LovesTech <alekslovestech@gmail.com>
2024-10-17 18:06:50 +0200

    make KeyboardPieSlice and PieSliceKey responsive

--- src/Components/KeyboardPieSlice.tsx
+++ src/Components/KeyboardPieSlice.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import "../styles/KeyboardPieSlice.css";
 import { TWELVE } from "../types/NoteConstants";
 import { ActualIndex } from "../types/IndexTypes";
@@ -9,34 +9,43 @@ import { useNotes } from "./NotesContext";
 import AccidentalToggle from "./AccidentalToggle";
 import CircularVisModeSelect from "./CircularVizModeSelect";
 import { PolarMath } from "../utils/PolarMath";
-import { CommonMath, INNER_RADIUS, MIDDLE_RADIUS, OUTER_RADIUS } from "../utils/CommonMath";
+import { CommonMath, OUTER_RADIUS } from "../utils/CommonMath";
 import { drawCircularVisualizationsSVG } from "./CircularVisualizationsSVG";
 
 interface PieSliceProps {
   index: number;
   onClick: () => void;
+  outerRadius: number;
+  middleRadius: number;
+  innerRadius: number;
 }
 
-const PieSliceKey: React.FC<PieSliceProps> = ({ index, onClick }) => {
+const PieSliceKey: React.FC<PieSliceProps> = ({
+  index,
+  onClick,
+  outerRadius,
+  middleRadius,
+  innerRadius,
+}) => {
   const { selectedNoteIndices, selectedAccidental } = useNotes();
   const { startAngle, middleAngle, endAngle } = CommonMath.NoteIndexToAngles(index);
 
-  //get the inner and outer radius from the width of the container (extract from the CSS)
-  const outerStart = PolarMath.getCartesianFromPolar(OUTER_RADIUS, startAngle);
-  const outerEnd = PolarMath.getCartesianFromPolar(OUTER_RADIUS, endAngle);
-  const innerStart = PolarMath.getCartesianFromPolar(INNER_RADIUS, startAngle);
-  const innerEnd = PolarMath.getCartesianFromPolar(INNER_RADIUS, endAngle);
+  //TODO: get the inner and outer radius from the width of the container (extract from the CSS)
+  const outerStart = PolarMath.getCartesianFromPolar(outerRadius, startAngle);
+  const outerEnd = PolarMath.getCartesianFromPolar(outerRadius, endAngle);
+  const innerStart = PolarMath.getCartesianFromPolar(innerRadius, startAngle);
+  const innerEnd = PolarMath.getCartesianFromPolar(innerRadius, endAngle);
 
   const blackWhiteClass = getBlackWhiteString(index as ActualIndex);
   const isSelected = selectedNoteIndices.includes(index as ActualIndex);
   const selectedClass = isSelected ? "selected" : "";
-  const textPosition = PolarMath.getCartesianFromPolar(MIDDLE_RADIUS, middleAngle);
+  const textPosition = PolarMath.getCartesianFromPolar(middleRadius, middleAngle);
 
   const pathData = [
     `M ${outerStart.x} ${outerStart.y}`,
-    `A ${OUTER_RADIUS} ${OUTER_RADIUS} 0 0 1 ${outerEnd.x} ${outerEnd.y}`,
+    `A ${outerRadius} ${outerRadius} 0 0 1 ${outerEnd.x} ${outerEnd.y}`,
     `L ${innerEnd.x} ${innerEnd.y}`,
-    `A ${INNER_RADIUS} ${INNER_RADIUS} 0 0 0 ${innerStart.x} ${innerStart.y}`,
+    `A ${innerRadius} ${innerRadius} 0 0 0 ${innerStart.x} ${innerStart.y}`,
     "Z",
   ].join(" ");
 
@@ -57,9 +66,38 @@ const KeyboardPieSlice: React.FC = () => {
     handleKeyClick(index as ActualIndex);
   };
 
+  const [outerRadius, setOuterRadius] = React.useState(OUTER_RADIUS);
+
   useEffect(() => {
-    drawCircularVisualizationsSVG(selectedNoteIndices, circularVisMode);
-  }, [selectedNoteIndices, handleKeyClick, circularVisMode]);
+    drawCircularVisualizationsSVG(selectedNoteIndices, circularVisMode, innerRadius);
+  }, [selectedNoteIndices, handleKeyClick, circularVisMode, outerRadius]);
+
+  useEffect(() => {
+    const updateDimensions = () => {
+      const containerWidth =
+        document.querySelector(".keyboardpieslice-container")?.clientWidth || 2 * OUTER_RADIUS;
+      console.log(`containerWidth: ${containerWidth}`);
+      const newOuterRadius = containerWidth / 4;
+      setOuterRadius(newOuterRadius);
+    };
+
+    updateDimensions(); // Initial calculation
+    window.addEventListener("resize", updateDimensions);
+
+    return () => {
+      window.removeEventListener("resize", updateDimensions);
+    };
+  }, []);
+
+  const { innerRadius, middleRadius } = useMemo(() => {
+    const innerRadius = outerRadius * 0.5;
+    const middleRadius = outerRadius * 0.75;
+    console.log(`inner: ${innerRadius}, middle: ${middleRadius}, outer: ${outerRadius}`);
+    return {
+      innerRadius,
+      middleRadius,
+    };
+  }, [outerRadius]);
 
   return (
     <div className="keyboardpieslice-container">
@@ -75,7 +113,14 @@ const KeyboardPieSlice: React.FC = () => {
       </div>
       <svg width="100%" height="100%" viewBox="-150 -150 300 300" className="keyboard-pieslice">
         {Array.from({ length: TWELVE }).map((_, index) => (
-          <PieSliceKey key={index} index={index} onClick={() => handleClick(index)} />
+          <PieSliceKey
+            key={index}
+            index={index}
+            onClick={() => handleClick(index)}
+            outerRadius={outerRadius}
+            middleRadius={middleRadius}
+            innerRadius={innerRadius}
+          />
         ))}
       </svg>
     </div>
