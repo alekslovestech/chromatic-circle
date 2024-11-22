a589509c2851dda0c6ac211318ec699a39f62d74
Aleks LovesTech <alekslovestech@gmail.com>
2024-11-22 07:01:55 +0100

    cleanup

--- src/Components/KeyboardLogo.tsx
+++ src/Components/KeyboardLogo.tsx
@@ -9,28 +9,26 @@ const KeyboardLogo: React.FC = () => {
   const innerRadius = 0.5 * outerRadius;
 
   return (
-    <div>
-      <div
-        id="svg-container"
-        style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}
+    <div
+      id="svg-container"
+      style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}
+    >
+      <svg
+        width={outerRadius * 2}
+        height={outerRadius * 2}
+        viewBox={`-${outerRadius} -${outerRadius} ${outerRadius * 2} ${outerRadius * 2}`}
+        stroke="black"
+        strokeWidth="1"
       >
-        <svg
-          width={outerRadius * 2}
-          height={outerRadius * 2}
-          viewBox={`-${outerRadius} -${outerRadius} ${outerRadius * 2} ${outerRadius * 2}`}
-          stroke="black"
-          strokeWidth="1"
-        >
-          {Array.from({ length: TWELVE }).map((_, index) => (
-            <PieSliceLogo
-              key={index}
-              actualIndex={index as ActualIndex}
-              outerRadius={outerRadius}
-              innerRadius={innerRadius}
-            />
-          ))}
-        </svg>
-      </div>
+        {Array.from({ length: TWELVE }).map((_, index) => (
+          <PieSliceLogo
+            key={index}
+            actualIndex={index as ActualIndex}
+            outerRadius={outerRadius}
+            innerRadius={innerRadius}
+          />
+        ))}
+      </svg>
     </div>
   );
 };
