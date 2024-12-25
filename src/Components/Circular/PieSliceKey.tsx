8260a1276f058032649343d7e9c9ad12a90d500f
Aleks LovesTech <alekslovestech@gmail.com>
2024-12-25 21:09:07 +0100

    refactor

--- src/Components/Circular/PieSliceKey.tsx
+++ src/Components/Circular/PieSliceKey.tsx
@@ -1,63 +1,25 @@
-import { ActualIndex } from "../../types/IndexTypes";
-import { TWELVE } from "../../types/NoteConstants";
-import { getBlackWhiteString } from "../../utils/ColorUtils";
-import { CommonMath } from "../../utils/CommonMath";
-import { getNoteTextFromIndex } from "../../utils/NoteUtils";
-import { PolarMath } from "../../utils/PolarMath";
+import React from "react";
 import { useNotes } from "../NotesContext";
+import PieSliceBase, { PieSliceBaseProps } from "./PieSliceBase";
 
-interface PieSliceProps {
-  actualIndex: ActualIndex;
-  onClick: () => void;
-  outerRadius: number;
-  innerRadius: number;
-  showText: boolean;
-}
-
-const PieSliceKey: React.FC<PieSliceProps> = ({
+const PieSliceKey: React.FC<PieSliceBaseProps> = ({
   actualIndex,
-  onClick,
   outerRadius,
   innerRadius,
-  showText,
+  onClick,
 }) => {
-  const { selectedNoteIndices, selectedAccidental } = useNotes();
-  const { startAngle, middleAngle, endAngle } = CommonMath.NoteIndexToAngles(actualIndex);
+  const { selectedNoteIndices } = useNotes();
 
-  const innerRadiusRounded = Math.round(innerRadius * 100) / 100;
-  const outerRadiusRounded = Math.round(outerRadius * 100) / 100;
-  const outerStart = PolarMath.getCartesianFromPolar(outerRadius, startAngle, true);
-  const outerEnd = PolarMath.getCartesianFromPolar(outerRadius, endAngle, true);
-  const innerStart = PolarMath.getCartesianFromPolar(innerRadius, startAngle, true);
-  const innerEnd = PolarMath.getCartesianFromPolar(innerRadius, endAngle, true);
-
-  const blackWhiteClass = getBlackWhiteString(actualIndex);
-
-  const isSelected0 = selectedNoteIndices.includes(actualIndex);
-  const isSelected1 = selectedNoteIndices.includes((actualIndex + TWELVE) as ActualIndex);
-  const isSelected = isSelected0 || isSelected1;
-
-  const selectedClass = isSelected ? "selected" : "";
-  const middleRadius = (innerRadius + outerRadius) / 2;
-  const textPosition = PolarMath.getCartesianFromPolar(middleRadius, middleAngle, true);
-
-  const pathData = [
-    `M ${outerStart.x} ${outerStart.y}`,
-    `A ${outerRadiusRounded} ${outerRadiusRounded} 0 0 1 ${outerEnd.x} ${outerEnd.y}`,
-    `L ${innerEnd.x} ${innerEnd.y}`,
-    `A ${innerRadiusRounded} ${innerRadiusRounded} 0 0 0 ${innerStart.x} ${innerStart.y}`,
-    "Z",
-  ].join(" ");
-
+  const isSelected = selectedNoteIndices.includes(actualIndex);
   return (
-    <g className={`pie-slice-key ${blackWhiteClass} ${selectedClass}`}>
-      <path d={pathData} onClick={onClick} />
-      {showText && (
-        <text x={textPosition.x} y={textPosition.y} textAnchor="middle" dominantBaseline="middle">
-          {getNoteTextFromIndex(actualIndex, selectedAccidental)}
-        </text>
-      )}
-    </g>
+    <PieSliceBase
+      actualIndex={actualIndex}
+      outerRadius={outerRadius}
+      innerRadius={innerRadius}
+      onClick={onClick}
+      isSelected={isSelected}
+      showText={true}
+    />
   );
 };
 
--- src/Components/Circular/PieSliceLogo.tsx
+++ src/Components/Circular/PieSliceLogo.tsx
@@ -1,35 +1,15 @@
-import { ActualIndex } from "../../types/IndexTypes";
-import { getBlackWhiteString } from "../../utils/ColorUtils";
-import { CommonMath } from "../../utils/CommonMath";
-import { PolarMath } from "../../utils/PolarMath";
+import React from "react";
+import PieSliceBase, { PieSliceBaseProps } from "./PieSliceBase";
 
-interface PieSliceLogoProps {
-  actualIndex: ActualIndex;
-  outerRadius: number;
-  innerRadius: number;
-}
-const PieSliceLogo: React.FC<PieSliceLogoProps> = ({ actualIndex, outerRadius, innerRadius }) => {
-  const { startAngle, endAngle } = CommonMath.NoteIndexToAngles(actualIndex);
-
-  const outerStart = PolarMath.getCartesianFromPolar(outerRadius, startAngle, true);
-  const outerEnd = PolarMath.getCartesianFromPolar(outerRadius, endAngle, true);
-  const innerStart = PolarMath.getCartesianFromPolar(innerRadius, startAngle, true);
-  const innerEnd = PolarMath.getCartesianFromPolar(innerRadius, endAngle, true);
-
-  const pathData = [
-    `M ${outerStart.x} ${outerStart.y}`,
-    `A ${outerRadius} ${outerRadius} 0 0 1 ${outerEnd.x} ${outerEnd.y}`,
-    `L ${innerEnd.x} ${innerEnd.y}`,
-    `A ${innerRadius} ${innerRadius} 0 0 0 ${innerStart.x} ${innerStart.y}`,
-    "Z",
-  ].join(" ");
-
-  const color = getBlackWhiteString(actualIndex);
-
+const PieSliceLogo: React.FC<PieSliceBaseProps> = ({ actualIndex, outerRadius, innerRadius }) => {
   return (
-    <g fill={color}>
-      <path d={pathData} />
-    </g>
+    <PieSliceBase
+      actualIndex={actualIndex}
+      outerRadius={outerRadius}
+      innerRadius={innerRadius}
+      isSelected={false}
+      showText={false}
+    />
   );
 };
 
