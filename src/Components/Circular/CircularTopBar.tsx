3bc2b4cec50f95395eca3e3bcc3898c5479bc40d
Aleks LovesTech <alekslovestech@gmail.com>
2025-02-10 13:32:55 +0100

    rename file

--- src/Components/Circular/CircularTopBar.tsx
+++ src/Components/Circular/CircularTopBar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import AccidentalToggle from "../AccidentalToggle";
 import CircularVisModeSelect from "./CircularVizModeSelect";
-import MusicKeySelector from "../MusicKeySelector";
+import MusicalKeySelector from "../MusicalKeySelector";
 
 export const CircularTopBar: React.FC = () => {
   return (
     <div id="keyboardcircular-topbar">
       {/* <AccidentalToggle /> */}
-      <MusicKeySelector />
+      <MusicalKeySelector />
       <CircularVisModeSelect />
     </div>
   );
---
+++ src/Components/MusicalKeySelector.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useNotes } from "./NotesContext";
+import { KeyType, MusicalKey, MusicalKeyUtil } from "../types/MusicalKey";
+
+const MusicalKeySelector: React.FC = () => {
+  const { setSelectedAccidental, setSelectedMusicalKey } = useNotes();
+
+  const [isMajor, setIsMajor] = React.useState(true);
+  const [localKey, setLocalKey] = React.useState<string>("C");
+  const majorKeys = MusicalKeyUtil.getMajorsList();
+  const minorKeys = MusicalKeyUtil.getMinorsList();
+  const keys = isMajor ? majorKeys : minorKeys;
+
+  React.useEffect(() => {
+    const newKey = new MusicalKey(localKey, getMajorMinorString(isMajor));
+    console.log("newKey", newKey);
+    setSelectedMusicalKey(newKey);
+    setSelectedAccidental(newKey.getDefaultAccidental());
+  }, [isMajor, localKey, setSelectedMusicalKey, setSelectedAccidental]);
+
+  const getMajorMinorString = (isMajor: boolean) => {
+    return isMajor ? KeyType.Major : KeyType.Minor;
+  };
+
+  const handleKeyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const key = event.target.value;
+    setLocalKey(key);
+  };
+
+  const handleMajorToggle = () => {
+    setIsMajor((prevIsMajor) => !prevIsMajor);
+  };
+
+  return (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <button onClick={handleMajorToggle} style={{ marginRight: "10px" }}>
+        {isMajor ? ":)" : ":("}
+      </button>
+      <select onChange={handleKeyChange}>
+        {keys.map((key) => (
+          <option key={key} value={key}>
+            {key}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default MusicalKeySelector;
