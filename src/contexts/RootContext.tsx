56c693a7fff3648696f349ab6e4c4b1bf7ec039d
Aleks LovesTech <alekslovestech@gmail.com>
2025-03-23 11:33:23 +0100

    tweaks

--- src/contexts/RootContext.tsx
+++ src/contexts/RootContext.tsx
@@ -4,12 +4,10 @@ import { DisplayProvider } from "./DisplayContext";
 import { MusicalProvider } from "./MusicalContext";
 import { PresetProvider } from "./PresetContext";
 
-export const RootProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <DisplayProvider>
-      <MusicalProvider>
-        <PresetProvider>{children}</PresetProvider>
-      </MusicalProvider>
-    </DisplayProvider>
-  );
-};
+export const RootProvider: React.FC<{ children: ReactNode }> = ({ children }) => (
+  <DisplayProvider>
+    <MusicalProvider>
+      <PresetProvider>{children}</PresetProvider>
+    </MusicalProvider>
+  </DisplayProvider>
+);
