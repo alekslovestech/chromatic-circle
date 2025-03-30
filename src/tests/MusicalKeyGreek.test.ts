80702b33dff0bbafbb26264d2dd940a79f2ce800
Aleks LovesTech <alekslovestech@gmail.com>
2025-03-30 20:20:47 +0200

    improved tests

--- src/tests/MusicalKeyGreek.test.ts
+++ src/tests/MusicalKeyGreek.test.ts
@@ -1,58 +1,68 @@
-import { KeyType, MusicalKey } from "../types/MusicalKey";
+import { noteTextToIndex } from "../types/ChromaticIndex";
+import { GreekModeType, MusicalKey } from "../types/MusicalKey";
 
-import { AccidentalType } from "../types/AccidentalType";
+//NB: here we don't care about the distinction between C# and Db, etc.
+//We just want to verify that the scale is correct.
+function verifyNoteList(musicalKey: MusicalKey, expectedNotes: string[]) {
+  const noteList = musicalKey.generateIndexArray();
+  const expectedIndices = expectedNotes.map((note) => noteTextToIndex(note));
+  expect(noteList).toEqual(expectedIndices);
+}
 
-const verifyMusicalKeyAccidental = (
-  key: string,
-  keyType: KeyType,
-  expectedAccidental: AccidentalType,
-) => {
-  const musicalKey = MusicalKey.fromClassicalMode(key, keyType);
-  expect(musicalKey.getDefaultAccidental()).toBe(expectedAccidental);
-};
+describe("Greek Mode Index Arrays", () => {
+  describe("Ionian (Major) Mode", () => {
+    const C_IONIAN_KEY = MusicalKey.fromGreekMode("C", GreekModeType.Ionian);
 
-describe("getNoteTextFromIndex", () => {
-  //sharp major keys
-  it("C major => sharp", () => {
-    verifyMusicalKeyAccidental("C", KeyType.Major, AccidentalType.Sharp);
+    test("has correct scale indexes", () => {
+      verifyNoteList(C_IONIAN_KEY, ["C", "D", "E", "F", "G", "A", "B"]);
+    });
   });
 
-  it("G major => sharp", () => {
-    verifyMusicalKeyAccidental("G", KeyType.Major, AccidentalType.Sharp);
+  describe("Dorian Mode", () => {
+    const D_DORIAN_KEY = MusicalKey.fromGreekMode("D", GreekModeType.Dorian);
+
+    test("has correct scale indexes", () => {
+      verifyNoteList(D_DORIAN_KEY, ["D", "E", "F", "G", "A", "B", "C"]);
+    });
   });
 
-  it("D minor => flat", () => {
-    verifyMusicalKeyAccidental("D", KeyType.Minor, AccidentalType.Flat);
+  describe("Phrygian Mode", () => {
+    const E_PHRYGIAN_KEY = MusicalKey.fromGreekMode("E", GreekModeType.Phrygian);
+
+    test("has correct scale indexes", () => {
+      verifyNoteList(E_PHRYGIAN_KEY, ["E", "F", "G", "A", "B", "C", "D"]);
+    });
   });
 
-  it("D major => sharp", () => {
-    verifyMusicalKeyAccidental("D", KeyType.Major, AccidentalType.Sharp);
+  describe("Lydian Mode", () => {
+    const F_LYDIAN_KEY = MusicalKey.fromGreekMode("F", GreekModeType.Lydian);
+
+    test("has correct scale indexes", () => {
+      verifyNoteList(F_LYDIAN_KEY, ["F", "G", "A", "B", "C", "D", "E"]);
+    });
   });
 
-  it("F# major => sharp", () => {
-    verifyMusicalKeyAccidental("F#", KeyType.Major, AccidentalType.Sharp);
-  });
+  describe("Mixolydian Mode", () => {
+    const G_MIXOLYDIAN_KEY = MusicalKey.fromGreekMode("G", GreekModeType.Mixolydian);
 
-  //flat major keys
-  it("F major => flat", () => {
-    verifyMusicalKeyAccidental("F", KeyType.Major, AccidentalType.Flat);
-  });
+    test("has correct scale indexes", () => {
+      verifyNoteList(G_MIXOLYDIAN_KEY, ["G", "A", "B", "C", "D", "E", "F"]);
+    });
 
-  it("Bb major => flat", () => {
-    verifyMusicalKeyAccidental("Bb", KeyType.Major, AccidentalType.Flat);
-  });
+    describe("Aeolian (Natural Minor) Mode", () => {
+      const A_AEOLIAN_KEY = MusicalKey.fromGreekMode("A", GreekModeType.Aeolian);
 
-  it("Eb major => flat", () => {
-    verifyMusicalKeyAccidental("Eb", KeyType.Major, AccidentalType.Flat);
-  });
+      test("has correct scale indexes", () => {
+        verifyNoteList(A_AEOLIAN_KEY, ["A", "B", "C", "D", "E", "F", "G"]);
+      });
+    });
 
-  //sharp minor keys
-  it("C# minor => sharp", () => {
-    verifyMusicalKeyAccidental("C#", KeyType.Minor, AccidentalType.Sharp);
-  });
+    describe("Locrian Mode", () => {
+      const B_LOCRIAN_KEY = MusicalKey.fromGreekMode("B", GreekModeType.Locrian);
 
-  //flat minor keys
-  it("F minor => flat", () => {
-    verifyMusicalKeyAccidental("F", KeyType.Minor, AccidentalType.Flat);
+      test("has correct scale indexes", () => {
+        verifyNoteList(B_LOCRIAN_KEY, ["B", "C", "D", "E", "F", "G", "A"]);
+      });
+    });
   });
 });
