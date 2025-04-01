d65fb7f595f9fa7235f335fbd004863825f97e17
Aleks LovesTech <alekslovestech@gmail.com>
2025-04-02 00:42:41 +0200

    fix bug

--- src/Components/Circular/NoteDisplayModeSelect.tsx
+++ src/Components/Circular/NoteDisplayModeSelect.tsx
@@ -26,8 +26,8 @@ export const KeyTextModeSelect: React.FC = () => {
           type="radio"
           id="key-text-numbers"
           name="key-text-mode"
-          checked={keyTextMode === KeyTextMode.Arabic}
-          onChange={() => setKeyTextMode(KeyTextMode.Arabic)}
+          checked={keyTextMode === KeyTextMode.ScaleDegree}
+          onChange={() => setKeyTextMode(KeyTextMode.ScaleDegree)}
         />
         <label htmlFor="key-text-numbers">1</label>
       </div>
