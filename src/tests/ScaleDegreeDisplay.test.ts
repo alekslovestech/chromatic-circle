1f4391dcc15626508ed59f700599f71e2ee78360
Aleks LovesTech <alekslovestech@gmail.com>
2025-04-03 00:15:33 +0200

    test refactoring

--- src/tests/ScaleDegreeDisplay.test.ts
+++ src/tests/ScaleDegreeDisplay.test.ts
@@ -1,32 +1,33 @@
+import { ixChromatic } from "../types/ChromaticIndex";
 import { GreekModeType } from "../types/GreekMode";
 import { MusicalKey } from "../types/MusicalKey";
 import { verifyGreekModeScaleDegrees, verifyScaleDegreesArray } from "./utils/DisplayTestUtils";
 
 describe("Greek Mode Index Arrays", () => {
   describe("verifyFromPattern", () => {
-    test("C Ionian mode - major scale pattern", () => {
+    test("Ionian mode pattern", () => {
       verifyGreekModeScaleDegrees(GreekModeType.Ionian, ["1", "2", "3", "4", "5", "6", "7"]);
     });
-    test("C Do rian mode - major scale pattern", () => {
+    test("Dorian mode pattern", () => {
       verifyGreekModeScaleDegrees(GreekModeType.Dorian, ["1", "2", "♭3", "4", "5", "6", "♭7"]);
     });
-    test("C Phrygian mode - major scale pattern", () => {
+    test("Phrygian mode pattern", () => {
       verifyGreekModeScaleDegrees(GreekModeType.Phrygian, ["1", "♭2", "♭3", "4", "5", "♭6", "♭7"]);
     });
 
-    test("C Lydian mode - major scale pattern", () => {
+    test("Lydian mode pattern", () => {
       verifyGreekModeScaleDegrees(GreekModeType.Lydian, ["1", "2", "3", "♯4", "5", "6", "7"]);
     });
 
-    test("C Mixolydian mode - major scale pattern", () => {
+    test("Mixolydian mode pattern", () => {
       verifyGreekModeScaleDegrees(GreekModeType.Mixolydian, ["1", "2", "3", "4", "5", "6", "♭7"]);
     });
 
-    test("C Aeolian mode - major scale pattern", () => {
+    test("Aeolian mode pattern", () => {
       verifyGreekModeScaleDegrees(GreekModeType.Aeolian, ["1", "2", "♭3", "4", "5", "♭6", "♭7"]);
     });
 
-    test("C Locrian mode - major scale pattern", () => {
+    test("Locrian mode pattern", () => {
       verifyGreekModeScaleDegrees(GreekModeType.Locrian, ["1", "♭2", "♭3", "4", "♭5", "♭6", "♭7"]);
     });
   });
@@ -49,6 +50,19 @@ describe("getScaleDegreeDisplayString", () => {
   });
 
   describe("Dorian Mode", () => {
+    it("C is a flat 7 in D Dorian", () => {
+      expect(dDorianKey.getScaleDegreeDisplayString(ixChromatic(0))).toEqual("♭7");
+    });
+    it("C is a 1 in C Dorian", () => {
+      expect(cDorianKey.getScaleDegreeDisplayString(ixChromatic(0))).toEqual("1");
+    });
+    it("B♭ does not exist in D Dorian", () => {
+      expect(dDorianKey.getScaleDegreeDisplayString(ixChromatic(10))).toEqual("");
+    });
+    it("B is a 6 in D Dorian", () => {
+      expect(dDorianKey.getScaleDegreeDisplayString(ixChromatic(11))).toEqual("6");
+    });
+
     it("should display correct scale degrees for C Dorian", () => {
       verifyScaleDegreesArray(cDorianKey, [
         "1",
