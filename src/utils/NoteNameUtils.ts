d00f67232c719f06ee9323a0b6a1bd5fda10e98b
Aleks LovesTech <alekslovestech@gmail.com>
2025-04-05 00:03:25 +0200

    tweaks

--- src/utils/NoteNameUtils.ts
+++ src/utils/NoteNameUtils.ts
@@ -1,17 +1,17 @@
-import { AccidentalType, getAccidentalSignForDisplay } from "../types/AccidentalType";
+import { AccidentalType } from "../types/AccidentalType";
 import { ActualIndex, actualIndexToChromaticAndOctave } from "../types/IndexTypes";
 import { ChromaticIndex } from "../types/ChromaticIndex";
 import { MusicalKey } from "../types/MusicalKey";
 import { KeyTextMode } from "../types/SettingModes";
 import { RomanNumeralUtils } from "./RomanNumeralUtils";
 import { NoteConverter } from "../types/NoteConverter";
+
 const formatNoteNameForDisplay = (
   chromaticIndex: ChromaticIndex,
   accidentalPreference: AccidentalType,
 ): string => {
   const noteAtIndex = NoteConverter.getBasicNoteInfo(chromaticIndex, accidentalPreference);
-  const accidentalSign = getAccidentalSignForDisplay(noteAtIndex.accidental);
-  return `${noteAtIndex.noteName}${accidentalSign}`;
+  return noteAtIndex.formatNoteNameForDisplay();
 };
 
 export const getNoteTextFromActualIndex = (
