d8a4f4cb9a422d38d8e7a003b5c1ea0932c7c400
Aleks LovesTech <alekslovestech@gmail.com>
2025-04-06 09:27:19 +0200

    diatonic color scheme drafts

--- src/Components/Circular/PieSlice.tsx
+++ src/Components/Circular/PieSlice.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { ChromaticIndex } from "../../types/ChromaticIndex";
 import { GlobalMode, KeyTextMode } from "../../types/SettingModes";
 
-import { PolarMath } from "../../utils/Circular/PolarMath";
 import { getBlackWhiteString } from "../../utils/ColorUtils";
 import { IndexUtils } from "../../utils/IndexUtils";
 import { isSelectedEitherOctave } from "../../utils/KeyboardUtils";
@@ -11,31 +10,7 @@ import { useDisplay } from "../../contexts/DisplayContext";
 import { useMusical } from "../../contexts/MusicalContext";
 import { MusicalKeyDisplay } from "../../types/Keys/MusicalKeyDisplay";
 import { MusicalKeyScale } from "../../types/Keys/MusicalKeyScale";
-
-const ROMAN_MODE = false; //TODO: make this a prop
-const ROMAN_POINT_COEFFICIENT = 0.85;
-const getArcPathFromIndex = (
-  chromaticIndex: ChromaticIndex,
-  outerRadius: number,
-  innerRadius: number,
-): JSX.Element => {
-  const { startAngle, endAngle } = PolarMath.NoteIndexToAngleRange(chromaticIndex);
-  // Convert angles to cartesian coordinates
-  const outerStart = PolarMath.getCartesianFromPolar(outerRadius, startAngle, true);
-  const outerEnd = PolarMath.getCartesianFromPolar(outerRadius, endAngle, true);
-  const innerStart = PolarMath.getCartesianFromPolar(innerRadius, startAngle, true);
-  const innerEnd = PolarMath.getCartesianFromPolar(innerRadius, endAngle, true);
-
-  // Create SVG path: move to outer start, arc to outer end, line to inner end, arc to inner start, close path
-  const arcPath = [
-    `M ${outerStart.x} ${outerStart.y}`, // Move to start
-    `A ${outerRadius} ${outerRadius} 0 0 1 ${outerEnd.x} ${outerEnd.y}`, // Outer arc
-    `L ${innerEnd.x} ${innerEnd.y}`, // Line to inner
-    `A ${innerRadius} ${innerRadius} 0 0 0 ${innerStart.x} ${innerStart.y}`, // Inner arc
-    "Z", // Close path
-  ].join(" ");
-  return <path d={arcPath} />;
-};
+import { ArcPathVisualizer } from "../../utils/Circular/ArcPathVisualizer";
 
 export const PieSlice: React.FC<{
   chromaticIndex: ChromaticIndex;
@@ -45,13 +20,12 @@ export const PieSlice: React.FC<{
 }> = ({ chromaticIndex, outerRadius, innerRadius, onClick }) => {
   const { selectedMusicalKey, selectedNoteIndices } = useMusical();
   const { monochromeMode, keyTextMode, globalMode } = useDisplay();
-  const pathElement = getArcPathFromIndex(chromaticIndex, outerRadius, innerRadius);
-  const middleAngle = PolarMath.NoteIndexToMiddleAngle(chromaticIndex);
-  const textPoint = PolarMath.getCartesianFromPolar((innerRadius + outerRadius) * 0.5, middleAngle);
-  const romanPoint = PolarMath.getCartesianFromPolar(
-    innerRadius * ROMAN_POINT_COEFFICIENT,
-    middleAngle,
+  const pathElement = ArcPathVisualizer.getArcPathFromIndex(
+    chromaticIndex,
+    outerRadius,
+    innerRadius,
   );
+  const textPoint = ArcPathVisualizer.getTextPoint(chromaticIndex, outerRadius, innerRadius);
   const blackWhiteString = monochromeMode ? "white" : getBlackWhiteString(chromaticIndex);
   const classNames = ["pie-slice-key", blackWhiteString];
   const isSelected =
@@ -75,25 +49,6 @@ export const PieSlice: React.FC<{
           </text>
         )}
       </g>
-      {ROMAN_MODE && (
-        <text
-          x={romanPoint.x}
-          y={romanPoint.y}
-          textAnchor="middle"
-          dominantBaseline="middle"
-          stroke="black"
-          strokeWidth="1"
-          fontSize="4px"
-        >
-          <tspan fontSize="8px">
-            {MusicalKeyDisplay.getDisplayString(
-              selectedMusicalKey,
-              chromaticIndex,
-              KeyTextMode.Roman,
-            )}
-          </tspan>
-        </text>
-      )}
     </>
   );
 };
