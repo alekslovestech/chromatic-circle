b4b5d5ff0a7099d05d3af9baf720a91f5b6368b9
Aleks LovesTech <alekslovestech@gmail.com>
2025-04-11 15:56:43 +0200

    tweaks

--- src/Components/Linear/PianoKey.tsx
+++ src/Components/Linear/PianoKey.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { ActualIndex } from "../../types/IndexTypes";
+import { ActualIndex, actualIndexToChromaticAndOctave } from "../../types/IndexTypes";
 import { NoteConverter } from "../../types/NoteConverter";
 import { MusicalKey } from "../../types/Keys/MusicalKey";
 
 import { IndexUtils } from "../../utils/IndexUtils";
-
-import { useDisplay } from "../../contexts/DisplayContext";
 import { isBlackKey } from "../../utils/KeyboardUtils";
 
+import { useDisplay } from "../../contexts/DisplayContext";
+
+const WHITE_KEYS_PER_OCTAVE = 7;
+const WHITE_KEYS_PER_2_OCTAVES = 14;
+const BLACK_TO_WHITE_RATIO = 0.7; // Black keys are 70% the width of white keys
 interface PianoKeyProps {
   actualIndex: ActualIndex;
   isSelected: boolean;
@@ -75,32 +78,46 @@ export const PianoKey: React.FC<PianoKeyProps> = ({
   );
 };
 
-const KEY_POSITIONS: { [key: number]: { position: number; isDarkKey: boolean } } = {
-  0: { position: 0, isDarkKey: false }, // C
-  1: { position: 1.0, isDarkKey: true }, // C#/Db
-  2: { position: 1, isDarkKey: false }, // D
-  3: { position: 2.0, isDarkKey: true }, // D#/Eb
-  4: { position: 2, isDarkKey: false }, // E
-  5: { position: 3, isDarkKey: false }, // F
-  6: { position: 4.0, isDarkKey: true }, // F#/Gb
-  7: { position: 4, isDarkKey: false }, // G
-  8: { position: 5.0, isDarkKey: true }, // G#/Ab
-  9: { position: 5, isDarkKey: false }, // A
-  10: { position: 6.0, isDarkKey: true }, // A#/Bb
-  11: { position: 6, isDarkKey: false }, // B
-  12: { position: 7, isDarkKey: false }, // C (next octave)
-  13: { position: 8.0, isDarkKey: true }, // C#/Db
-  14: { position: 8, isDarkKey: false }, // D
-  15: { position: 9.0, isDarkKey: true }, // D#/Eb
-  16: { position: 9, isDarkKey: false }, // E
-  17: { position: 10, isDarkKey: false }, // F
-  18: { position: 11.0, isDarkKey: true }, // F#/Gb
-  19: { position: 11, isDarkKey: false }, // G
-  20: { position: 12.0, isDarkKey: true }, // G#/Ab
-  21: { position: 12, isDarkKey: false }, // A
-  22: { position: 13.0, isDarkKey: true }, // A#/Bb
-  23: { position: 13, isDarkKey: false }, // B
-};
+function getKeyPosition(actualIndex: ActualIndex): {
+  position: number;
+  isConventionallyBlackKey: boolean;
+} {
+  let pos = -1;
+  const { chromaticIndex, octaveOffset } = actualIndexToChromaticAndOctave(actualIndex);
+  switch (chromaticIndex) {
+    case 0:
+      pos = 0;
+      break;
+    case 1:
+    case 2:
+      pos = 1;
+      break;
+    case 3:
+    case 4:
+      pos = 2;
+      break;
+    case 5:
+      pos = 3;
+      break;
+    case 6:
+    case 7:
+      pos = 4.0;
+      break;
+    case 8:
+    case 9:
+      pos = 5.0;
+      break;
+    case 10:
+    case 11:
+      pos = 6;
+      break;
+    default:
+      pos = -1;
+  }
+  pos += octaveOffset * WHITE_KEYS_PER_OCTAVE;
+
+  return { position: pos, isConventionallyBlackKey: isBlackKey(actualIndex) };
+}
 
 /**
  * Calculates the position of a piano key based on its index and whether it's black or white.
@@ -113,27 +130,13 @@ const calculateKeyRect = (
   isConventionallyBlack: boolean,
   containerWidth: number,
 ): { left: number; width: number } => {
-  // Constants for positioning
-  const WHITE_KEYS_PER_2_OCTAVES = 14; // 14 white keys in 2 octaves
-  const BLACK_TO_WHITE_RATIO = 0.7; // Black keys are 70% the width of white keys
-
   // Calculate the width of white and black keys
   const whiteKeyWidth = containerWidth / WHITE_KEYS_PER_2_OCTAVES;
   const blackKeyWidth = whiteKeyWidth * BLACK_TO_WHITE_RATIO;
 
-  const keyInfo = KEY_POSITIONS[actualIndex];
+  const keyInfo = getKeyPosition(actualIndex);
 
-  if (isConventionallyBlack) {
-    const blackKeyPosition = keyInfo.position * whiteKeyWidth - blackKeyWidth / 2;
-    return {
-      left: blackKeyPosition,
-      width: blackKeyWidth,
-    };
-  } else {
-    const whiteKeyPosition = keyInfo.position * whiteKeyWidth;
-    return {
-      left: whiteKeyPosition,
-      width: whiteKeyWidth,
-    };
-  }
+  const width = isConventionallyBlack ? blackKeyWidth : whiteKeyWidth;
+  const left = keyInfo.position * whiteKeyWidth - (isConventionallyBlack ? blackKeyWidth / 2 : 0);
+  return { left, width };
 };
