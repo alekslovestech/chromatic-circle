46a6a56f8042ea8545f909a6348427bff77ed520
Aleks LovesTech <alekslovestech@gmail.com>
2025-05-07 12:28:44 +0200

    CSS now working properly, so is routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,10 @@
 "use client";
 
-import React, { useEffect, Suspense } from "react";
-import { useSearchParams } from "next/navigation";
-
-import AudioPlayer from "../components/AudioPlayer";
-import { KeyboardCircular } from "../components/Keyboard/Circular/KeyboardCircular";
-import { CircularSettings } from "../components/Keyboard/Circular/CircularSettings";
-import { KeyboardLinear } from "../components/Keyboard/Linear/KeyboardLinear";
-import SettingsContainer from "../components/Settings/SettingsContainer";
-import StaffRenderer from "../components/StaffRenderer";
-
+import React, { Suspense } from "react";
 import { RootProvider } from "../contexts/RootContext";
-import { GlobalMode, useGlobal } from "../contexts/GlobalContext";
-
-const AppContent: React.FC = () => {
-  const borderStyle = { border: `1px solid var(--debug-border-color)` };
-  const { globalMode, setGlobalMode } = useGlobal();
-  const searchParams = useSearchParams();
-  const mode = searchParams.get("mode");
-
-  useEffect(() => {
-    if (mode === "logo") {
-      setGlobalMode(GlobalMode.Logo);
-    } else if (mode === "advanced") {
-      setGlobalMode(GlobalMode.Advanced);
-    } else {
-      setGlobalMode(GlobalMode.Default);
-    }
-  }, [mode, setGlobalMode]);
-
-  return (
-    <div className="grid-container">
-      <div className="keyboardlinear-container" style={borderStyle}>
-        <KeyboardLinear />
-      </div>
-      <div className="keyboardcircular-container" style={borderStyle}>
-        <KeyboardCircular />
-        {globalMode !== GlobalMode.Logo && <CircularSettings />}
-      </div>
-      <SettingsContainer />
-      <StaffRenderer />
-    </div>
-  );
-};
+import { GlobalMode } from "../contexts/GlobalContext";
+import AppContent from "../components/AppContent";
+import AudioPlayer from "../components/AudioPlayer";
 
 export default function Home() {
   return (
--- components/AppContent.tsx
+++ components/AppContent.tsx
@@ -1,27 +1,17 @@
 "use client";
 
 import React from "react";
-import { useSearchParams } from "next/navigation";
 import { KeyboardCircular } from "./Keyboard/Circular/KeyboardCircular";
 import { CircularSettings } from "./Keyboard/Circular/CircularSettings";
 import { KeyboardLinear } from "./Keyboard/Linear/KeyboardLinear";
 import SettingsContainer from "./Settings/SettingsContainer";
 import StaffRenderer from "./StaffRenderer";
-import { GlobalMode, useGlobal } from "../contexts/GlobalContext";
+import { useGlobal } from "../contexts/GlobalContext";
+import { GlobalMode } from "../contexts/GlobalContext";
 
 const AppContent: React.FC = () => {
   const borderStyle = { border: `1px solid var(--debug-border-color)` };
-  const { setGlobalMode } = useGlobal();
-  const searchParams = useSearchParams();
-  const mode = searchParams.get("mode");
-
-  React.useEffect(() => {
-    if (mode === "advanced") {
-      setGlobalMode(GlobalMode.Advanced);
-    } else {
-      setGlobalMode(GlobalMode.Default);
-    }
-  }, [mode, setGlobalMode]);
+  const { globalMode } = useGlobal();
 
   return (
     <div className="grid-container">
@@ -30,7 +20,7 @@ const AppContent: React.FC = () => {
       </div>
       <div className="keyboardcircular-container" style={borderStyle}>
         <KeyboardCircular />
-        <CircularSettings />
+        {globalMode !== GlobalMode.Logo && <CircularSettings />}
       </div>
       <SettingsContainer />
       <StaffRenderer />
